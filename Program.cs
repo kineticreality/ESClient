c7fb78c83b7b4799b5109dd66b77a88d586eeec4
Mike Reid <mike.reid@xero.com>
2017-10-03 15:56:12 +1300

    Whee

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        
+        private static SessionStatsReadModel _sessionStatsRm;
+        private static ConsoleView _consoleView;
+
+        private const string StreamName = "Account";
+        private const string ReadModelFile = "AccountCheckpoint.csv";
+
+        static void Main()
+        {
+            Console.Write("Connecting to Event Store");
+
+            EventStoreLoader.SetupEventStore();
+
+			var privateCopy = Guid.NewGuid() + ".csv";
+			if (File.Exists(ReadModelFile))
+				File.Copy(ReadModelFile, privateCopy);
+
+			_consoleView = new ConsoleView();
+
+
+			_sessionStatsRm = new SessionStatsReadModel(_consoleView);
+
+			
+
+			if (File.Exists(privateCopy))
+			{
+				File.Copy(privateCopy, ReadModelFile, true);
+				File.Delete(privateCopy);
+			}
+
+
+        }
+
+    }
+}
